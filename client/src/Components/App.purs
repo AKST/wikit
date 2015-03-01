f7512b38f08001786ed37607c4c6f8c9bd85bc89
Angus Thomsen <aksthomsen@gmail.com>
2015-03-02 09:23:56 +1000

    refactor app into module

---
+++ client/src/Components/App.purs
@@ -0,0 +1,22 @@
+module Components.App (app) where
+
+
+import qualified Thermite as T
+import qualified Thermite.Html as T
+import qualified Thermite.Html.Elements as T
+import qualified Thermite.Html.Attributes as A
+import qualified Thermite.Action as T
+import qualified Thermite.Events as T
+import qualified Thermite.Types as T
+
+
+app = T.createClass (T.simpleSpec {} performAction render) where
+
+  performAction _ _ = do
+    T.modifyState \_ -> {}
+
+  render ctx _ _ = T.div [A._id "app", A.className "container"] [
+    T.h1' [T.text "Hello World"],
+    T.p'  [T.text "Welcome to my home page (-:"]
+  ]
+
--- client/src/Main.purs
+++ client/src/Main.purs
@@ -1,31 +1,15 @@
 module Main where
 
 
-import Debug.Trace (trace, Trace(..))
-
 import Control.Monad.Eff
 import qualified Control.Monad.JQuery as J
 
 import qualified Thermite as T
-import qualified Thermite.Html as T
-import qualified Thermite.Html.Elements as T
-import qualified Thermite.Html.Attributes as A
-import qualified Thermite.Action as T
-import qualified Thermite.Events as T
-import qualified Thermite.Types as T
+
+import Components.App
 
 
 main = J.ready do
   T.render app {}
 
-
-app = T.createClass (T.simpleSpec {} performAction render) where
-
-  performAction _ _ = do
-    T.modifyState \_ -> {}
-
-  render ctx _ _ = T.div [A._id "app", A.className "container"] [
-    T.h1' [T.text "Hello World"],
-    T.p'  [T.text "Welcome to my home page (-:"]
-  ]
 
